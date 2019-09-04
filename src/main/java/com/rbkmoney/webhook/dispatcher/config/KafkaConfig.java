349cc6f2eccb380b1449a44d60646e46f6baf179
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-09-04 17:46:28 +0300

    Fix timeout

--- src/main/java/com/rbkmoney/webhook/dispatcher/config/KafkaConfig.java
+++ src/main/java/com/rbkmoney/webhook/dispatcher/config/KafkaConfig.java
@@ -55,7 +55,6 @@ public class KafkaConfig {
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST);
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 1000);
         sslConfigure(props);
         return props;
     }
