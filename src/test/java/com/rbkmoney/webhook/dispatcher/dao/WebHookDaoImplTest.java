146b917ad06a0108fde3fc85c6263ba80972d8db
a.romanov <a.romanov@rbkmoney.com>
2020-09-10 12:59:04 +0300

    BJ-976: Provide tests for WebHookDao

--- src/test/java/com/rbkmoney/webhook/dispatcher/dao/WebHookDaoImplTest.java
+++ src/test/java/com/rbkmoney/webhook/dispatcher/dao/WebHookDaoImplTest.java
@@ -1,48 +1,72 @@
 package com.rbkmoney.webhook.dispatcher.dao;
 
+import com.rbkmoney.webhook.dispatcher.WebhookDispatcherApplication;
 import com.rbkmoney.webhook.dispatcher.WebhookMessage;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
+import com.rbkmoney.webhook.dispatcher.repository.DeadHookRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Slf4j
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(classes = {WebHookDaoImpl.class, FlywayAutoConfiguration.class})
+@SpringBootTest(classes = WebhookDispatcherApplication.class)
 public class WebHookDaoImplTest extends DaoTestBase {
 
     @Autowired
     private WebHookDao webHookDao;
 
+    @Autowired
+    private DeadHookRepository deadHookRepository;
+
     @Test
-    public void pgTest() throws InterruptedException {
-        Thread.sleep(20000L);
+    public void shouldCommitWebHooks() {
+        // Given
         WebhookMessage webHook = new WebhookMessage();
         webHook.setSourceId("test");
         webHook.setEventId(1);
         webHook.setUrl("/test");
         webHook.setParentEventId(-1);
+
+        // When
         webHookDao.commit(webHook);
 
+        // Then
         webHook.setParentEventId(1);
-
         boolean commitParent = webHookDao.isParentCommitted(webHook);
-
-        Assert.assertTrue(commitParent);
+        assertTrue(commitParent);
 
         boolean commit = webHookDao.isCommitted(webHook);
-
-        Assert.assertTrue(commit);
+        assertTrue(commit);
 
         webHook.setEventId(666L);
-        Assert.assertFalse(webHookDao.isCommitted(webHook));
-
+        assertFalse(webHookDao.isCommitted(webHook));
     }
 
+    @Test
+    public void shouldBuryWebHooks() {
+        // Given
+        WebhookMessage webHook = new WebhookMessage();
+        webHook.setWebhookId(0L);
+        webHook.setSourceId("source");
+        webHook.setEventId(1L);
+        webHook.setParentEventId(2L);
+        webHook.setCreatedAt("2016-03-22T06:12:27Z");
+        webHook.setUrl("/url");
+        webHook.setContentType("contentType");
+        webHook.setAdditionalHeaders(Map.of("a", "b"));
+        webHook.setRequestBody("body".getBytes());
+        webHook.setRetryCount(3L);
+
+        // When
+        webHookDao.bury(webHook);
+
+        // Then
+        assertTrue(deadHookRepository.findById("0_source_1").isPresent());
+    }
 }
