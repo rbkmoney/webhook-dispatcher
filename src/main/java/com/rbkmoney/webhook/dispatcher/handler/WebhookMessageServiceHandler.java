b433d36d3488800f58d488bd7ec6f4182b42f07d
a.romanov <a.romanov@rbkmoney.com>
2020-09-10 18:12:26 +0300

    BJ-976: Add handler, service and servlet

---
+++ src/main/java/com/rbkmoney/webhook/dispatcher/handler/WebhookMessageServiceHandler.java
@@ -0,0 +1,21 @@
+package com.rbkmoney.webhook.dispatcher.handler;
+
+import com.rbkmoney.webhook.dispatcher.WebhookMessageServiceSrv;
+import com.rbkmoney.webhook.dispatcher.service.WebhookMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebhookMessageServiceHandler implements WebhookMessageServiceSrv.Iface {
+
+    private final WebhookMessageService webhookMessageService;
+
+    @Override
+    public void resend(
+            long webhookId,
+            String sourceId,
+            long eventId) {
+        webhookMessageService.resend();
+    }
+}
---
+++ src/main/java/com/rbkmoney/webhook/dispatcher/service/WebhookMessageService.java
@@ -0,0 +1,13 @@
+package com.rbkmoney.webhook.dispatcher.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebhookMessageService {
+
+    public void resend() {
+        // TODO [a.romanov]: impl
+    }
+}
