99c505b3a35ad87a4ec376fb2c2e188508901425
a.romanov <a.romanov@rbkmoney.com>
2020-09-10 12:59:04 +0300

    BJ-976: Provide tests for WebHookDao

--- src/main/java/com/rbkmoney/webhook/dispatcher/converter/CommitLogConverter.java
+++ src/main/java/com/rbkmoney/webhook/dispatcher/converter/CommitLogConverter.java
@@ -1,9 +1,12 @@
 package com.rbkmoney.webhook.dispatcher.converter;
 
+import com.rbkmoney.geck.common.util.TypeUtil;
 import com.rbkmoney.webhook.dispatcher.WebhookMessage;
 import com.rbkmoney.webhook.dispatcher.entity.CommitLogEntity;
 import com.rbkmoney.webhook.dispatcher.utils.IdGenerator;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class CommitLogConverter {
@@ -14,6 +17,7 @@ public class CommitLogConverter {
                         webhookMessage.getWebhookId(),
                         webhookMessage.getSourceId(),
                         webhookMessage.getEventId()))
+                .creationTime(TypeUtil.toLocalDateTime(Instant.now()))
                 .build();
     }
 }
