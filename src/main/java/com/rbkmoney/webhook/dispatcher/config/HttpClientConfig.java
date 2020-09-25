8ec69123d8866f916eb63cfcc9b060b7dfcd4080
Kostya <kennny007@yandex.ru>
2020-09-25 10:18:36 +0300

    Fix retry events (#25)

--- src/main/java/com/rbkmoney/webhook/dispatcher/config/HttpClientConfig.java
+++ src/main/java/com/rbkmoney/webhook/dispatcher/config/HttpClientConfig.java
@@ -2,6 +2,7 @@ package com.rbkmoney.webhook.dispatcher.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.NoConnectionReuseStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -31,6 +32,7 @@ public class HttpClientConfig {
                 .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
                 .setMaxConnPerRoute(maxPerRoute)
                 .setMaxConnTotal(maxTotal)
+                .setConnectionReuseStrategy(new NoConnectionReuseStrategy())
                 .build();
     }
 
